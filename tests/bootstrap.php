7d21bf813d60cbf9c17a11c621ef3cc82b4c81d8
Alexey Abel <dev@abelonline.de>
2018-04-29 22:25:40 +0200

    Remove unnecessary code

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,10 +8,4 @@ require_once __DIR__.'/../../../lib/base.php';
 
 \OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
 
-if (!class_exists('\PHPUnit\Framework\TestCase') &&
-	class_exists('\PHPUnit_Framework_TestCase')) {
-	class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
-    require_once('../vendor/autoload.php');
-}
-
 OC_Hook::clear();
